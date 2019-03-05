a4c7db9284efe7bf5d528a9372b6d16b55b0b294
MohammedFaidey <41902166+Mohammed-Faidey@users.noreply.github.com>
2019-03-06 00:13:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ class Auth:
     using Google
     """
     # OAuth credentials
-    CLIENT_ID = '<insert client id here>'
-    CLIENT_SECRET = '<insert client secret here>'
+    CLIENT_ID = '<494683665446-r08617vleja50ht306krubjlclu3mi1h.apps.googleusercontent.com>'
+    CLIENT_SECRET = '<cnprTYSQrLkSvy-V0Dc5wULz>'
     # URI that google server will redirect to
-    REDIRECT_URI = 'http://localhost:5000/gCallback'
+    REDIRECT_URI = '"http://localhost:8000"'
     # Endpoint to start OAuth request from
     AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
     # Endpoint to fetch user token
-    TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
+    TOKEN_URI = 'https://oauth2.googleapis.com/token'
     # Endpoint to get user information at the end of oauth
     USER_INFO = 'https://www.googleapis.com/userinfo/v2/me'
     # Data we plan to access from Google profile
